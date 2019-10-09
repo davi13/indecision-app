7f9e06bb8e5be5db9040c4612081bd18b5fdd27c
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-09 08:20:05 +0200

    configuring webpack entry->output

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+//entry->output
+console.log());
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+}
