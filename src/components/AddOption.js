031b12d20f46fc08b4ab86e8f5225dced4402bfb
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-10 09:54:38 +0200

    added some devtool to webpack to mapping errors

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -10,7 +10,7 @@ export default class AddOption extends React.Component {
     }
     handleAddOption(e) {
         e.preventDefault();
-
+        console.log(testing);
         const option = e.target.elements.options.value.trim()
         const error = this.props.handleAddOption(option);
         // this.setState(() => {
